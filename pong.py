a463b15c789eededdbda04a2f1e96851659ad95a
shrouk <shroukalaa242@gmail.com>
2023-11-05 11:26:47 +0200

    pong.py updated

--- pong.py
+++ pong.py
@@ -1,9 +1,14 @@
 import turtle
+import winsound
 wn=turtle.Screen()
 wn.title("Pong game")
 wn.bgcolor("black")
 wn.setup(width=800,height=600)
 wn.tracer(0)
+# Score
+score_a = 0
+score_b = 0
+
 # Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
@@ -32,6 +37,16 @@ ball.penup()
 ball.goto(0, 0)
 ball.dx=2
 ball.dy=-2
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color('white')
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align='center', font=('Courier', 24,'normal'))
+
 # Function
 def paddle_a_up():
     y = paddle_a.ycor()
@@ -70,16 +85,31 @@ while True:
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
+        winsound.PlaySound("mixkit",winsound.SND_ASYNC)
     elif ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
+        winsound.PlaySound("mixkit",winsound.SND_ASYNC)
     if ball.xcor() > 390:
         ball.goto(0,0)
         ball.dx*=-1
+        score_a+=1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a,score_b), align='center', font=('Courier', 24,'normal'))
     elif ball.xcor() <- 390:
         ball.goto(0,0)
+        ball.dx*=-1 
+        score_b+=1   
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a,score_b), align='center', font=('Courier', 24,'normal'))
+    # Paddle and ball collision
+    if (ball.xcor() > 340 and ball.xcor() < 350)and (ball.ycor()<paddle_b.ycor()+40 and ball.ycor()>paddle_b.ycor()-40):
+        ball.setx(340)
+        ball.dx*=-1
+    if (ball.xcor() < -340 and ball.xcor() > -350)and (ball.ycor()<paddle_a.ycor()+40 and ball.ycor()>paddle_a.ycor()-40):
+        ball.setx(340)
         ball.dx*=-1    
-
+    
 
     
         
